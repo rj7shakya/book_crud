ff9e9ff60b0f904925e43f599826d3495f9318fc
rajad shakya <rajadshakya@rajads-MacBook-Air.local>
2024-01-16 15:31:57 +0545

    setup code

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+const books = [
+  { title: "A", author: "B" },
+  { title: "C", author: "D" },
+  // ... more books
+];
+
+app.get("/books", (req, res) => {
+  res.render("books", { books: books });
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+app.post("/books", (req, res) => {
+  res.send("books post");
+});
+
+app.listen(3000, () => {
+  console.log("listening");
+});
