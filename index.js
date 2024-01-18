cb12523e3d1d5e196a66663978414fcc16bb8425
rajad shakya <rajadshakya@rajads-MacBook-Air.local>
2024-01-18 14:41:39 +0545

    day3 pg connection

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 
+const { Client } = require("pg");
+const client = new Client({
+  user: "postgres",
+  host: "localhost",
+  database: "books",
+  password: "inspiron",
+  port: "5432",
+});
+
+client.connect((err) => {
+  if (err) {
+    console.error("Connection error:", err.stack);
+  } else {
+    console.log("Connected to PostgreSQL database!");
+  }
+});
+
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static("public"));
@@ -13,7 +30,11 @@ const books = [
 ];
 
 app.get("/books", (req, res) => {
-  res.render("books", { books: books });
+  // res.render("books", { books: books });
+  client
+    .query("SELECT * FROM books")
+    .then((resp) => res.render("books", { books: resp.rows }))
+    .catch((err) => console.log(err));
 });
 
 app.get("/add", (req, res) => {
@@ -21,8 +42,15 @@ app.get("/add", (req, res) => {
 });
 
 app.post("/books", (req, res) => {
-  books.push(req.body);
-  res.redirect("/books");
+  client
+    .query(`INSERT INTO books (title, author) VALUES ($1, $2)`, [
+      req.body.title,
+      req.body.author,
+    ])
+    .then((resp) => res.redirect("/books"))
+    .catch((err) => console.log(err));
+  // books.push(req.body);
+  // res.redirect("/books");
 });
 
 app.listen(3000, () => {
